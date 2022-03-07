b5e401ac6a2e94d11a0b3f9362597f07925c79f1
munfira-munas <munfimunas@gmail.com>
2022-03-07 09:27:11 +0530

    master file updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -191,9 +191,10 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4 ">
 
             <a href="index3.html" class="brand-link">
-                <img src="{{ URL('/storage/download.png') }}" alt="Ad Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
-                <span class="brand-text font-weight-light">Quote Software</span>
+                {{-- <img src="{{ URL('/storage/download.png') }}" alt="Ad Logo" class="brand-image img-circle elevation-3"
+                    style="opacity: .8"> --}}
+                <span class="brand-text font-weight-light">&nbsp;&nbsp;&nbsp;&nbsp;
+                    Quote Software</span>
             </a>
 
             <div class="sidebar">
